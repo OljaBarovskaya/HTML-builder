f47cb94c5748df98c5ca60c6a955a0b4364bf0d9
OljaBarovskaya <asdfg1234_08@mail.ru>
2023-05-06 16:48:42 +0300

    fix: correct "ctrl + c" command task2

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -9,10 +9,14 @@ fs.appendFile(neededPath, '', function (err) {
 const { stdin, stdout } = process;
 stdout.write('Приветствую Вас! Введите текст \n');
 
-process.on('exit', () => stdout.write('Удачи в изучении Node.js!'));
+process.on('SIGINT', ()=> {
+  stdout.write('Удачи в изучении Node.js!');
+  process.exit(0);
+});
 
 stdin.on('data', data => {
   if (data.toString().trim() == 'exit'){
+    stdout.write('Удачи в изучении Node.js!')
     process.exit();
   } else {
     fs.appendFile(neededPath, data, function (err) {
