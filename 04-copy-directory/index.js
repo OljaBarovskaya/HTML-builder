0db4022b21d1513355163b4efce932fd188c0b5b
OljaBarovskaya <asdfg1234_08@mail.ru>
2023-05-08 01:07:30 +0300

    feat: implement task4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,43 @@
+let fs = require('fs');
+let path = require('path');
+
+let toPath = path.resolve(__dirname, 'files-copy');
+let fromPath = path.resolve(__dirname,  'files' );
+
+fs.mkdir(toPath, () => {
+  console.log('Directory created successfully!');
+});
+
+fs.readdir(fromPath,  { withFileTypes: true }, (err, filesFrom) => { 
+  if (err) 
+    {console.log(err);} 
+    let arrNames = [];
+  filesFrom.forEach(file => {
+   let fromFullPath = path.resolve(__dirname,  'files', file.name );
+   let toFullPath = path.resolve(__dirname, 'files-copy',file.name);
+   arrNames.push(file.name)
+    fs.copyFile(fromFullPath, toFullPath, () =>
+    console.log(`file ${fromFullPath} copied to ${toFullPath}`));
+  })
+  
+  fs.readdir(toPath,  { withFileTypes: true }, (err, filesTo) => { 
+    if (err) 
+    {console.log(err);}
+    filesTo.forEach(file => {
+      if(!arrNames.includes(file.name)){
+        let toFullPath = path.resolve(__dirname, 'files-copy', file.name);
+        fs.unlink(toFullPath,   err => {
+          if(err) throw err;
+        })
+      }
+    }) 
+  })    
+})      
+
+
+
+
+
+
+
 
